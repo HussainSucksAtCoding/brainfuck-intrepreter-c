ac29519735bcdd2df14475a63186f0a53813d28e
HussainSucksAtCoding <hussain0ama@gmail.com>
2025-09-20 18:25:31 +0300

    added an executer with unsuccussful loop implementation and a missing input implementation

--- main.c
+++ main.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+//magic numbers
+#define code_end 9
 
 typedef enum {
-	move_right,
-	move_left,
-	increment,
-	decrement,
-	print,
-	loop_begin,
-	loop_end,
-	input,
+	MOVE_RIGHT,
+	MOVE_LEFT,
+	INCREMENT,
+	DECREMENT,
+	PRINT,
+	LOOP_BEGIN,
+	LOOP_END,
+	INPUT,
 } TokenType;
 
 long int fsize(FILE *f) {
@@ -21,55 +25,112 @@ long int fsize(FILE *f) {
 
 TokenType* lexer(FILE *source_file) {
 	size_t i = 0;
-	long int f_size = fsize(source_file);
-	char cmds[f_size];
+	long int file_length = fsize(source_file);
+	char source_chars[file_length];
 	
 	char c;
 
 	while( (c = fgetc(source_file)) != EOF) {
 		if(c != '\n') {
-			cmds[i] = (char)c;
+			source_chars[i] = (char)c;
 			i++;
 		}
 
 	}
 	
-	cmds[i] = '\0';
+	source_chars[i] = '\0';
 	int code_length = i;
 
-	TokenType tokens[code_length];
+	TokenType* tokens = malloc(sizeof(source_chars) * code_length);
 	for(int i = 0; i < code_length; i++) {
-		if(cmds[i] == '+') tokens[i] = increment;
-		if(cmds[i] == '-') tokens[i] = decrement;
+		if(source_chars[i] == '+') tokens[i] = INCREMENT;
+		if(source_chars[i] == '-') tokens[i] = DECREMENT;
 
-		if(cmds[i] == '>') tokens[i] = move_right;
-		if(cmds[i] == '<') tokens[i] = move_left;
+		if(source_chars[i] == '>') tokens[i] = MOVE_RIGHT;
+		if(source_chars[i] == '<') tokens[i] = MOVE_LEFT;
 
-		if(cmds[i] == '.') tokens[i] = print;
-		if(cmds[i] == ',') tokens[i] = input;
+		if(source_chars[i] == '.') tokens[i] = PRINT;
+		if(source_chars[i] == ',') tokens[i] = INPUT;
 
-		if(cmds[i] == '[') tokens[i] = loop_begin;
-		if(cmds[i] == ']') tokens[i] = loop_end;
+		if(source_chars[i] == '[') tokens[i] = LOOP_BEGIN;
+		if(source_chars[i] == ']') tokens[i] = LOOP_END;
 
-		printf("%c", cmds[i]);
+		printf("%c", source_chars[i]);
 	}
 
 	printf("\n");
 	
 	for(int i = 0; i < code_length; i++) {
-		printf("%d", tokens[i]);
+		//printf("%d", tokens[i]);
 	}
-
+	
+	tokens[i] = code_end;
 	return tokens;
 
 }
 
+void execute(TokenType *tokens) {
+	char code[1000] = {0};
+	size_t ptr = 0;
+
+	int loops = 0;
+	size_t loop_numeraters[100] = {0};
+	size_t loop_starts[100] = {0};
+	int numeraters = 0;
+	int current_loop = -1;
+
+	int i = 0;
+	while(tokens[i] != code_end) {
+		switch(tokens[i]) {
+			case MOVE_RIGHT:
+				ptr += 1;
+				break;
+			case MOVE_LEFT:
+				if(ptr != 0) {
+					ptr -= 1;
+				}
+				break;
+			case INCREMENT:
+				code[ptr] += 1;
+				break;
+			case DECREMENT:
+				code[ptr] -= 1;
+				break;
+			case LOOP_BEGIN:
+				loop_starts[current_loop] += i;
+				loop_numeraters[numeraters] = i - 1;
+				current_loop += 1;
+				numeraters += 1;
+				break;
+
+			case LOOP_END:
+				if(loops > 0 && numeraters > 0 && loop_numeraters[current_loop] >= 0) {
+						loop_numeraters[current_loop] -= 1;
+						ptr = loop_starts[current_loop] + 1;
+				}
+				break;
+			case PRINT:
+				printf("%d", code[ptr]);
+				break;
+			case INPUT:
+				//printf("taking input isnt implemented yet");
+		}
+
+		i++;
+	}
+}
+
 int main() {
 	FILE *f_ptr = fopen("code.bf", "r");
-	long int f_size = fsize(f_ptr);
-	char code_tape[f_size];
 
-	lexer(f_ptr);
+	if(f_ptr == NULL) {
+		printf("file not found");
+		exit(-1);
+	}
+
+	TokenType *tokens = lexer(f_ptr);
+	
+	execute(tokens);
 
 	fclose(f_ptr);
 	return 0;
